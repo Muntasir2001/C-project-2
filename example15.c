1711e659790f980c680d883c6140122725307c08
Muntasir2001 <52258261+Muntasir2001@users.noreply.github.com>
2019-10-18 16:20:08 +0100

    Commit 2
    example15 has been updated to  function as calculating the area of a rectangle in both metres and centimetres and .exe has been created.

--- example15.c
+++ example15.c
@@ -1,1 +1,13 @@
-// This program will calculate rectangles z
+// This program will calculate rectangles area and then convert the result to another unit.
+#include <stdio.h>
+#define M_CM 100
+
+int main() 
+{
+    float s1, s2; //defining s1 and s2 as two sides of a rectangle
+    printf("To calculate the area of the rectangle, please input the dimensions of both sides \n");
+    scanf("%f %f", &s1, &s2);
+    printf("The area of a rectangle in metres is %.2f \n", s1*s2); //calculating area in metres
+    printf("The area of a rectangle in cm is %.2f \n", s1 * s2 * M_CM); //converting m to cm
+    return 0;
+}
