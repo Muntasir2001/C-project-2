5f604eb8d3d08fa7c84e04230894dc57b066d8a9
Muntasir2001 <52258261+Muntasir2001@users.noreply.github.com>
2019-10-18 21:09:52 +0100

    Third commit
    Comments were added to example 16

--- example16.c
+++ example16.c
@@ -1,21 +1,21 @@
 //This program will do addition, multiplication, subtraction and division. We are going to do square root as well.
 #include <stdio.h>
-#include <math.h>
+#include <math.h> //math library. The math.h header defines various mathematical functions and one macro. All the functions available in this library take double as an argument and return double as the result.
 
 int main () 
 {
-    float a, b;
-    printf("Please enter a value for a \n");
+    float a, b; //variable a and b declared...
+    printf("Please enter a value for a \n"); //Inputting value a
     scanf("%f", &a);
-    printf("Please enter a value for b \n");
+    printf("Please enter a value for b \n"); //Inputing value b
     scanf("%f", &b);
 
-    printf("%.2f + %.2f = %.2f \n", a, b, a+b);
-    printf("%.2f - %.2f = %.2f \n", a, b, a-b);
-    printf("%.2f * %.2f = %.2f \n", a, b, a*b);
-    printf("%.2f / %.2f = %.2f \n", a, b, a/b);
+    printf("%.2f + %.2f = %.2f \n", a, b, a+b); //Displaying Addition of a and b
+    printf("%.2f - %.2f = %.2f \n", a, b, a-b); //Displaying Subtraction of a and b
+    printf("%.2f * %.2f = %.2f \n", a, b, a*b); //Displaying Multiplication of a and b
+    printf("%.2f / %.2f = %.2f \n", a, b, a/b); //Displaying Division of a and b
 
-    printf("%.2f to the power of %.2f is %.2f \n", a, b, pow(a,b));
-    printf("Square root of %.2f is %.2f \n", a, sqrt(a));
-    printf("Square root of %.2f is %.2f \n", b, sqrt(b));
+    printf("%.2f to the power of %.2f is %.2f \n", a, b, pow(a,b)); //displaying a to the power of b
+    printf("Square root of %.2f is %.2f \n", a, sqrt(a)); //Displaying square root of a
+    printf("Square root of %.2f is %.2f \n", b, sqrt(b)); //Displaying square root of b
 }
